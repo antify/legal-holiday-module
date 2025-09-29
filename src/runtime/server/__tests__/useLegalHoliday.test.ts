d38dc6c40ba201e200d06f2c5d72f07b298d3e02
max <maximilian@blue-panda.de>
2025-09-29 11:13:36 +0200

    chore: Add tests

---
+++ src/runtime/server/__tests__/useLegalHoliday.test.ts
@@ -0,0 +1,10 @@
+import {
+  describe, test, expect, vi, beforeEach,
+} from 'vitest';
+
+describe('useLegalHoliday test', async () => {
+
+  test('should get legal holidays', async () => {
+    // TODO: Implement test and mock api inside of the composable
+  });
+});
